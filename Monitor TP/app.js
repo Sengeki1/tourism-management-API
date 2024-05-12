f461c658bfe9607aa14a78fec190366a233ff222
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2024-05-12 00:38:52 -0100

    Add files via upload

--- Monitor TP/app.js
+++ Monitor TP/app.js
@@ -6,15 +6,17 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.post('/', (req, res) => {
+app.post('/transaction', (req, res) => {
     console.log(req.body)
 
-    for (let i = 0; i < req.body.length; i++) {
+    Object.keys(req.body).forEach(key => {
         // extract data 
         // validate it
         // send querys transaction to the other server depending of the transaction choice
-    }
+    })
 })
+
+// send STATUS CODE to the client refering if the transaction was successfull
 
 const server = http.createServer(app)
 let port = 5843
