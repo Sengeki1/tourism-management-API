faaa0fcc60be4de49a23a19dde8eae2f68dc23a6
Sengeki1 <gamersengeki@gmail.com>
2024-05-13 13:03:13 -0100

    Http Request added

--- Flight-API/app.py
+++ Flight-API/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3
+import baseDeDados as BD  # Importe o módulo baseDeDados.py
 
 app = Flask(__name__)
 
@@ -9,30 +10,27 @@ def conectar_bd():
     conn.row_factory = sqlite3.Row
     return conn
 
-# Rota para listar passageiros
+# Chame a função de criação de tabelas de baseDeDados.py para garantir que as tabelas sejam criadas antes de acessá-las
+BD.criar_tabelas()
+
+# Resto do código do app.py
+# Rotas para listar passageiros e voos disponíveis
 @app.route('/listar_passageiros')
 def listar_passageiros():
     conn = conectar_bd()
     cursor = conn.cursor()
-
     cursor.execute('''SELECT * FROM Passageiros''')
     passageiros = cursor.fetchall()
-
     conn.close()
-
     return render_template('passageiros.html', passageiros=passageiros)
 
-# Rota para listar voos disponíveis
 @app.route('/listar_voos_disponiveis')
 def listar_voos_disponiveis():
     conn = conectar_bd()
     cursor = conn.cursor()
-
     cursor.execute('''SELECT * FROM Voos WHERE vagas > 0''')
     voos = cursor.fetchall()
-
     conn.close()
-
     return render_template('voos_disponiveis.html', voos=voos)
 
 if __name__ == '__main__':
