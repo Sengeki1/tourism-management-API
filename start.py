292877cb17d47aba8479afbe4aae85003300ed2b
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2024-05-14 00:22:14 -0100

    Delete start.py

--- start.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.Popen(["gnome-terminal", "--", "bash", "-c", "cd 'Monitor TP' && npm run start"])
-subprocess.Popen(["gnome-terminal", "--", "bash", "-c", "cd Hotel-API/FastAPI && uvicorn --host=0.0.0.0 run:app --reload"])
-subprocess.Popen(["gnome-terminal", "--", "bash", "-c", "python3 app.py"])
-subprocess.Popen(["gnome-terminal", "--", "bash", "-c", "cd CLI && node."])
